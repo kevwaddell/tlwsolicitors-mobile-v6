9bf6674ff0a662ffd9ec5a939269c06fbefd46b7
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-20 10:40:42 +0100

    Move top hidden nav btn to top right

--- parts/navigation/nav-main.php
+++ parts/navigation/nav-main.php
@@ -8,11 +8,12 @@ $contact_pg = get_page_id( 'contact-us' );
 		
 		<div class="container-fluid">
 			
-		<ul class="top-nav-icon-links list-inline text-center">
-					<li><a href="<?php echo get_option('home'); ?>/"><i class="fa fa-home fa-2x"></i><span class="sr-only">Home</span></a></li>
-					<li><a href="mailto:<?php echo $email; ?>?subject=Enquiry from the <?php bloginfo('name'); ?> website"><i class="fa fa-envelope fa-2x"></i><span class="sr-only">Email Us</span></a></li>
-					<li><a href="<?php echo get_permalink($contact_pg); ?>#find-us"><i class="fa fa-map-marker fa-2x"></i><span class="sr-only">Find us</span></a></li>
-		</ul>
+		<div class="top-nav-icon-links">
+			<a href="<?php echo get_option('home'); ?>/"><i class="fa fa-home fa-2x"></i><span class="sr-only">Home</span></a>
+			<a href="mailto:<?php echo $email; ?>?subject=Enquiry from the <?php bloginfo('name'); ?> website"><i class="fa fa-envelope fa-2x"></i><span class="sr-only">Email Us</span></a>
+			<a href="<?php echo get_permalink($contact_pg); ?>#find-us"><i class="fa fa-map-marker fa-2x"></i><span class="sr-only">Find us</span></a>
+			<button id="close-nav" class="btn"><i class="fa fa-times"></i><span class="sr-only">Close navigation</span></button>
+		</div>
 			
 		<?php wp_nav_menu(array( 
 		'container' => 'false', 
@@ -24,5 +25,4 @@ $contact_pg = get_page_id( 'contact-us' );
 		</div>
 		
 	</div>
-	<button id="close-nav" class="btn"><i class="fa fa-times"></i><span class="sr-only">Close navigation</span></button>
 </nav>
