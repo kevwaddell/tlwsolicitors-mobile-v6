dc9c6f694001f1f763a1ad76b5d306192f0e54fa
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-07-20 15:43:06 +0100

    fief link in footer.

--- parts/footers/copyright-notice.php
+++ parts/footers/copyright-notice.php
@@ -7,3 +7,5 @@
 </div>
 
 <?php wp_nav_menu(array( 'container' => '', 'menu_class' => 'text-center social-links list-inline', 'theme_location' => 'social_links_menu', 'fallback_cb' => false ) ); ?>
+
+<a href="https://ww2.feefo.com/reviews/tlw-solicitors" class="feefo-logo" target="_blank"><img alt="Feefo logo" border="0" src="https://api.feefo.com/api/logo?merchantidentifier=tlw-solicitors" title="See what our customers say about us"/></a>
