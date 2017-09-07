4f3be7aab7a285e40fbb9d86825d079ac3bd0666
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-09-07 11:22:12 +0100

    Fix page not showing

--- parts/landingPage/page-masthead.php
+++
@@ -1,6 +0,0 @@
-<header class="header lp-header" role="banner">
-	<div class="container-fluid">
-		<a href="<?php echo get_option('home'); ?>/" class="logo text-hide"><?php bloginfo('name'); ?></a>		
-	</div>
-	<?php get_template_part( 'parts/global/color', 'strip' ); ?>			
-</header>
