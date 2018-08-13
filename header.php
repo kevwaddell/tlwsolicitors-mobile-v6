598b9fdc89b6b206f846938e94dc993ebc859da2
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-08-13 16:44:35 +0100

    Remove redundant adwords scripts

--- header.php
+++ header.php
@@ -9,13 +9,6 @@ $freephone_num = get_field('freephone_num', 'option');
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PLBR4F" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager -->
-<!-- Trinity Mirror Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHL78WL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Trinity Mirror Google Tag Manager (noscript) -->
-<script type="text/javascript">
-(function(a,e,c,f,g,h,b,d){var k={ak:"966769174",cl:"RtWJCOeI51gQlvT-zAM"};a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)};a[g]||(a[g]=k.ak);b=e.createElement(h);b.async=1;b.src="//www.gstatic.com/wcm/loader.js";d=e.getElementsByTagName(h)[0];d.parentNode.insertBefore(b,d);a[f]=function(b,d,e){a[c](2,b,k,d,null,new Date,e)};a[f]()})(window,document,"_googWcmImpl","_googWcmGet","_googWcmAk","script");
-</script>
 <?php } else { ?>
 <body <?php body_class($body_classes); ?>>
 <?php } ?>
